31c9ae33731887f46a16edf46acc61a3b240be62
AkashP27 <akashphayade27@gmail.com>
2023-10-23 14:00:30 +0530

    axios

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,52 +1,106 @@
-import React, { useContext } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useContext, useState } from "react";
 import Modal from "../UI/Modal";
 import CartContext from "../../context/cart-context";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
+import Checkout from "./Checkout";
+import axios from "axios";
 
 const Cart = (props) => {
- const cartCtx = useContext(CartContext);
+	const [isCheckout, setIsCheckout] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [didSubmit, setDidSubmit] = useState(false);
+	const cartCtx = useContext(CartContext);
 
- const cartItemRemoveHandler = (id) => {
-  cartCtx.removeItem(id);
- };
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
 
- const cartItemAddHandler = (item) => {
-  cartCtx.addItem({ ...item, amount: 1 });
- };
+	const cartItemAddHandler = (item) => {
+		cartCtx.addItem({ ...item, amount: 1 });
+	};
 
- const cartItems = (
-  <ul className={classes["cart-items"]}>
-   {cartCtx.items.map((item) => (
-    <CartItem
-     key={item.id}
-     name={item.name}
-     amount={item.amount}
-     price={item.price}
-     onRemove={cartItemRemoveHandler.bind(null, item.id)}
-     onAdd={cartItemAddHandler.bind(null, item)}
-    />
-   ))}
-  </ul>
- );
+	const orderHandler = () => {
+		setIsCheckout(true);
+	};
 
- return (
-  <Modal onClose={props.onCloseCart}>
-   {cartItems}
-   <div className={classes.total}>
-    <span>Total Amount</span>
-    <span>{`$${cartCtx.totalAmount.toFixed(2)}`}</span>
-   </div>
-   <div className={classes.actions}>
-    <button className={classes["button--alt"]} onClick={props.onCloseCart}>
-     Close
-    </button>
-    {cartCtx.items.length > 0 && (
-     <button className={classes.button}>Order</button>
-    )}
-   </div>
-  </Modal>
- );
+	const submitOrderHandler = async (userData) => {
+		setIsSubmitting(true);
+		const response = await axios.post(
+			"https://react-7da58-default-rtdb.firebaseio.com/orders.json",
+			{
+				user: userData,
+				orderedItems: cartCtx.items,
+			}
+		);
+		setIsSubmitting(false);
+		setDidSubmit(true);
+		cartCtx.clearCart();
+	};
+
+	const cartItems = (
+		<ul className={classes["cart-items"]}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
+
+	const modalActions = (
+		<div className={classes.actions}>
+			<button className={classes["button--alt"]} onClick={props.onCloseCart}>
+				Close
+			</button>
+			{cartCtx.items.length > 0 && (
+				<button className={classes.button} onClick={orderHandler}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
+	const cartModalContent = (
+		<React.Fragment>
+			{cartItems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>{`$${cartCtx.totalAmount.toFixed(2)}`}</span>
+			</div>
+			{isCheckout && (
+				<Checkout onConfirm={submitOrderHandler} onCancel={props.onCloseCart} />
+			)}
+			{!isCheckout && modalActions}
+		</React.Fragment>
+	);
+
+	const isSubmittingModalContent = <p>Sending order data...</p>;
+
+	const didSubmitModalContent = (
+		<React.Fragment>
+			<p>Successfully sent the order!</p>
+			<div className={classes.actions}>
+				<button className={classes.button} onClick={props.onCloseCart}>
+					Close
+				</button>
+			</div>
+		</React.Fragment>
+	);
+
+	return (
+		<Modal onClose={props.onCloseCart}>
+			{!isSubmitting && !didSubmit && cartModalContent}
+			{isSubmitting && isSubmittingModalContent}
+			{!isSubmitting && didSubmit && didSubmitModalContent}
+		</Modal>
+	);
 };
 
 export default Cart;
